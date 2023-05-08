f0c560b1412eaa65c9f84fa76d4d303c9c0f69d4
thasleem99 <shaikthasleem2000@gmail.com>
2023-05-08 15:29:42 +0530

    Crud Project

---
+++ crudEx/src/main/java/com/crud/crudEx/CrudExApplication.java
@@ -0,0 +1,13 @@
+package com.crud.crudEx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudExApplication.class, args);
+	}
+
+}
---
+++ crudEx/src/test/java/com/crud/crudEx/CrudExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.crudEx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
